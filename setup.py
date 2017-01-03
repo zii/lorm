1b0d18f07b6bbff68fcf7c68d1f0b8d0e961eb38
zii <wangchenguang@tbkt.cn>
2017-01-03 16:03:19 +0800

    python3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7+',
+        'Programming Language :: Python :: 2.7',
     ],
     install_requires = ['pymysql'],
     py_modules = ['lorm'],
