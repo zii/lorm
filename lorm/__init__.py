c2d6114a5c36df8776a665bbf110badd91b6ffc5
zii <gamcat@gmail.com>
2017-10-25 16:26:43 +0800

    fix coding issue

--- lorm/__init__.py
+++ lorm/__init__.py
@@ -1,5 +1,5 @@
 #coding: utf-8
 
-__version__ = '0.3.19'
+__version__ = '0.3.20'
 
 from .db import *
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 from setuptools import setup, find_packages
 
-__version__ = '0.3.19'
+__version__ = '0.3.20'
 
 setup(
     name         = "lorm",
